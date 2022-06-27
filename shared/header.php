e579dc43c8b754e05d2a981fcd438d97d48e9abe
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2022-06-27 01:56:46 -0700

    add some changes

---
+++ shared/header.php
@@ -0,0 +1,19 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="design/style.css">
+        <title>Library System</title>
+    </head>
+    <body>
+        <header class="header-nav">
+            <h1 class="header-name">Library System</h1>
+            <nav>
+                <a class="btn" href="http://">Books</a>
+                <a class="btn" href="http://">Theses</a>
+                <a class="btn" href="http://">Journals</a>
+            </nav>
+        </header>
+
+    
